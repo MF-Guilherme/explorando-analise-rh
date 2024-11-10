73c05adc34b9355e11f68389bb15f8c3ac96049e
Guilherme Montenegro <guimontenegro23@yahoo.com.br>
2024-11-09 22:53:57 -0300

    Desafio 3 - Calculando folha de pagamento - concluido

--- Program.cs
+++ Program.cs
@@ -1,27 +1,27 @@
 ﻿using System;
+using System.Linq;
 
-class Programa
+class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        // Entrada das notas
+        // Leitura da entrada como uma string
         string entrada = Console.ReadLine();
-        
-        // Separar os valores e convertê-los para inteiros
-        string[] notasString = entrada.Split(',');
-        int produtividade = int.Parse(notasString[0]);
-        int qualidade = int.Parse(notasString[1]);
-        int pontualidade = int.Parse(notasString[2]);
-        
-        // TODO: Calcule a média
-        double media = (produtividade + qualidade + pontualidade) / 3;
-        
-        // TODO: Verifique a elegibilidade para bônus
-        
-        string elegivelParaBonus = (media >= 7 ? "Sim" : "Nao");
-        
-        // Exibir os resultados
-        Console.WriteLine($"Media: {media}");
-        Console.WriteLine($"Elegivel para bonus: {elegivelParaBonus}");
+
+        // Separar a string e converter para uma lista de números
+        decimal[] valores = entrada.Split(',').Select(decimal.Parse).ToArray();
+
+        // Atribuir os valores às variáveis correspondentes
+        decimal salarioBase = valores[0];
+        int horasExtras = (int)valores[1];
+        decimal valorHoraExtra = valores[2];
+        decimal descontoIR = valores[3];
+        decimal descontoINSS = valores[4];
+
+        // TODO: Calcule o salário líquido
+        decimal salarioLiquido = salarioBase + (horasExtras * valorHoraExtra) - descontoIR - descontoINSS;
+
+        // Exibição do resultado
+        Console.WriteLine($"{salarioLiquido:F2}");
     }
 }
